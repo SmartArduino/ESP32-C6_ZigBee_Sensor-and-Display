27b11568acdb7aaf2bd5972a486d8ef5231bf061
Jorrit Ohm <94465481+jorritohm@users.noreply.github.com>
2024-09-17 22:40:05 +0200

    shorter code

---
+++ ZigBee Sensor/minimal_temp_sensor_zigbee/minimal_temp_sensor_zigbee.ino
@@ -0,0 +1,220 @@
+// Copyright 2024 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @brief This example demonstrates simple Zigbee temperature sensor.
+ */
+
+#include "esp_zigbee_core.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "ha/esp_zigbee_ha_standard.h"
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
+
+#define BME_SCK 14
+#define BME_MISO 15
+#define BME_MOSI 21
+#define BME_CS 20
+
+Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
+
+/* Default End Device config */
+#define ESP_ZB_ZED_CONFIG()                                                                 \
+  {                                                                                         \
+    .esp_zb_role = ESP_ZB_DEVICE_TYPE_ED, .install_code_policy = INSTALLCODE_POLICY_ENABLE, \
+    .nwk_cfg = {                                                                            \
+      .zed_cfg =                                                                            \
+        {                                                                                   \
+          .ed_timeout = ED_AGING_TIMEOUT,                                                   \
+          .keep_alive = ED_KEEP_ALIVE,                                                      \
+        },                                                                                  \
+    },                                                                                      \
+  }
+
+#define ESP_ZB_DEFAULT_RADIO_CONFIG() \
+  { .radio_mode = ZB_RADIO_MODE_NATIVE, }
+
+#define ESP_ZB_DEFAULT_HOST_CONFIG() \
+  { .host_connection_mode = ZB_HOST_CONNECTION_MODE_NONE, }
+
+#define INSTALLCODE_POLICY_ENABLE   false
+#define ED_AGING_TIMEOUT            ESP_ZB_ED_AGING_TIMEOUT_64MIN
+#define ED_KEEP_ALIVE               3000                                 /* 3000 millisecond */
+#define HA_ESP_SENSOR_ENDPOINT      10                                   /* temperature sensor endpoint */
+#define ESP_ZB_PRIMARY_CHANNEL_MASK ESP_ZB_TRANSCEIVER_ALL_CHANNELS_MASK /* Zigbee primary channel mask */
+
+/* Temperature sensor configuration */
+#define ESP_TEMP_SENSOR_UPDATE_INTERVAL (1)  /* Local sensor update interval (second) */
+#define ESP_TEMP_SENSOR_MIN_VALUE       (10) /* Local sensor min measured value (degree Celsius) */
+#define ESP_TEMP_SENSOR_MAX_VALUE       (50) /* Local sensor max measured value (degree Celsius) */
+
+/********************* Zigbee functions **************************/
+static int16_t zb_temperature_to_s16(float temp) {
+  return (int16_t)(temp * 100);
+}
+
+static void esp_app_temp_sensor_handler(float temperature) {
+  int16_t measured_value = zb_temperature_to_s16(temperature);
+  Serial.println("Updating temperature sensor value...");
+  Serial.println(measured_value);
+  /* Update temperature sensor measured value */
+  esp_zb_lock_acquire(portMAX_DELAY);
+  esp_zb_zcl_set_attribute_val(
+    HA_ESP_SENSOR_ENDPOINT, ESP_ZB_ZCL_CLUSTER_ID_TEMP_MEASUREMENT, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE, ESP_ZB_ZCL_ATTR_TEMP_MEASUREMENT_VALUE_ID, &measured_value,
+    false
+  );
+  esp_zb_lock_release();
+}
+
+void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct) {
+  uint32_t *p_sg_p = signal_struct->p_app_signal;
+  esp_err_t err_status = signal_struct->esp_err_status;
+  esp_zb_app_signal_type_t sig_type = (esp_zb_app_signal_type_t)*p_sg_p;
+  switch (sig_type) {
+    case ESP_ZB_ZDO_SIGNAL_SKIP_STARTUP:
+      log_i("Zigbee stack initialized");
+      esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_INITIALIZATION);
+      break;
+    case ESP_ZB_BDB_SIGNAL_DEVICE_FIRST_START:
+    case ESP_ZB_BDB_SIGNAL_DEVICE_REBOOT:
+      if (err_status == ESP_OK) {
+        log_i("Start network steering");
+        xTaskCreate(temp_sensor_value_update, "temp_sensor_update", 2048, NULL, 10, NULL);
+        if (esp_zb_bdb_is_factory_new()) {
+          esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_NETWORK_STEERING);
+        }
+      } else {
+        log_w("Failed to initialize Zigbee stack (status: %s)", esp_err_to_name(err_status));
+      }
+      break;
+    case ESP_ZB_BDB_SIGNAL_STEERING:
+      if (err_status == ESP_OK) {
+        log_i("Joined network successfully");
+      } else {
+        log_i("Network steering was not successful (status: %s)", esp_err_to_name(err_status));
+        esp_zb_scheduler_alarm((esp_zb_callback_t)esp_zb_bdb_start_top_level_commissioning, ESP_ZB_BDB_MODE_NETWORK_STEERING, 1000);
+      }
+      break;
+    default: log_i("ZDO signal: %s, status: %s", esp_zb_zdo_signal_to_string(sig_type), esp_err_to_name(err_status)); break;
+  }
+}
+
+static esp_zb_cluster_list_t *custom_temperature_sensor_clusters_create(esp_zb_temperature_sensor_cfg_t *temperature_sensor) {
+  esp_zb_cluster_list_t *cluster_list = esp_zb_zcl_cluster_list_create();
+  ESP_ERROR_CHECK(esp_zb_cluster_list_add_temperature_meas_cluster(
+    cluster_list, esp_zb_temperature_meas_cluster_create(&(temperature_sensor->temp_meas_cfg)), ESP_ZB_ZCL_CLUSTER_SERVER_ROLE
+  ));
+  return cluster_list;
+}
+
+static esp_zb_ep_list_t *custom_temperature_sensor_ep_create(uint8_t endpoint_id, esp_zb_temperature_sensor_cfg_t *temperature_sensor) {
+  esp_zb_ep_list_t *ep_list = esp_zb_ep_list_create();
+  esp_zb_endpoint_config_t endpoint_config = {
+    .endpoint = endpoint_id, .app_profile_id = ESP_ZB_AF_HA_PROFILE_ID, .app_device_id = ESP_ZB_HA_TEMPERATURE_SENSOR_DEVICE_ID, .app_device_version = 0
+  };
+  esp_zb_ep_list_add_ep(ep_list, custom_temperature_sensor_clusters_create(temperature_sensor), endpoint_config);
+  return ep_list;
+}
+
+static void esp_zb_task(void *pvParameters) {
+  esp_zb_cfg_t zb_nwk_cfg = ESP_ZB_ZED_CONFIG();
+  esp_zb_init(&zb_nwk_cfg);
+  esp_zb_temperature_sensor_cfg_t sensor_cfg = ESP_ZB_DEFAULT_TEMPERATURE_SENSOR_CONFIG();
+  sensor_cfg.temp_meas_cfg.min_value = zb_temperature_to_s16(ESP_TEMP_SENSOR_MIN_VALUE);
+  sensor_cfg.temp_meas_cfg.max_value = zb_temperature_to_s16(ESP_TEMP_SENSOR_MAX_VALUE);
+  esp_zb_ep_list_t *esp_zb_sensor_ep = custom_temperature_sensor_ep_create(HA_ESP_SENSOR_ENDPOINT, &sensor_cfg);
+  esp_zb_device_register(esp_zb_sensor_ep);
+
+  esp_zb_zcl_reporting_info_t reporting_info = {
+    .direction = ESP_ZB_ZCL_CMD_DIRECTION_TO_SRV,
+    .ep = HA_ESP_SENSOR_ENDPOINT,
+    .cluster_id = ESP_ZB_ZCL_CLUSTER_ID_TEMP_MEASUREMENT,
+    .cluster_role = ESP_ZB_ZCL_CLUSTER_SERVER_ROLE,
+    .attr_id = ESP_ZB_ZCL_ATTR_TEMP_MEASUREMENT_VALUE_ID,
+    .u =
+      {
+        .send_info =
+          {
+            .min_interval = 1,
+            .max_interval = 5,
+            .delta =
+              {
+                .u16 = 10,
+              },
+            .def_min_interval = 1,
+            .def_max_interval = 5,
+          },
+      },
+    .dst =
+      {
+        .profile_id = ESP_ZB_AF_HA_PROFILE_ID,
+      },
+    .manuf_code = ESP_ZB_ZCL_ATTR_NON_MANUFACTURER_SPECIFIC,
+  };
+  esp_zb_zcl_update_reporting_info(&reporting_info);
+  esp_zb_set_primary_network_channel_set(ESP_ZB_PRIMARY_CHANNEL_MASK);
+  ESP_ERROR_CHECK(esp_zb_start(false));
+  esp_zb_main_loop_iteration();
+}
+
+/************************ Temp sensor *****************************/
+static void temp_sensor_value_update(void *arg) {
+  for (;;) {
+    float tsens_value = bme.temperature;
+    esp_app_temp_sensor_handler(tsens_value);
+    delay(1000);  // Send the temperature every second
+  }
+}
+
+/********************* Arduino functions **************************/
+void setup() {
+  Serial.begin(115200);
+
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    while (1);
+  }
+  bme.setTemperatureOversampling(BME680_OS_8X);
+
+  // Init Zigbee
+  esp_zb_platform_config_t config = {
+    .radio_config = ESP_ZB_DEFAULT_RADIO_CONFIG(),
+    .host_config = ESP_ZB_DEFAULT_HOST_CONFIG(),
+  };
+  ESP_ERROR_CHECK(esp_zb_platform_config(&config));
+
+
+  // Start Zigbee task
+  xTaskCreate(esp_zb_task, "Zigbee_main", 4096, NULL, 5, NULL);
+}
+
+void loop() {
+    if (!bme.performReading()) {
+        Serial.println("Failed to perform reading :(");
+        return;
+    }
+
+    // Aktuelle Temperatur auslesen
+    float temperature = bme.temperature;
+
+    // Temperaturwert an den ZigBee-Koordinator übermitteln
+    esp_app_temp_sensor_handler(temperature);
+
+    // 1 Sekunde warten
+    delay(1000);
+}
